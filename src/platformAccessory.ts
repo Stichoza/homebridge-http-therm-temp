a77c5d79a412fdecc445cba945e38f80396a59d2
Levan Velijanashvili <stichoza@gmail.com>
2025-11-29 00:58:28 +0400

    Refactor code, remove examples, add real usage

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,60 +1,45 @@
 import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
+import type { HttpThermostatTemperaturePlatform } from './platform.js';
 
-import type { ExampleHomebridgePlatform } from './platform.js';
-
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
+export class HttpThermostatTemperatureAccessory {
   private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private state = {
+    On: true,
+    Temperature: 23,
+    TargetTemperature: 23,
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: HttpThermostatTemperaturePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Stichoza')
+      .setCharacteristic(this.platform.Characteristic.Model, 'HTTP Thermostat Temperature')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, '1.0.0');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
+    this.service = this.accessory.getService(this.platform.Service.Thermostat) || this.accessory.addService(this.platform.Service.Thermostat);
 
-    if (accessory.context.device.CustomService) {
-      // This is only required when using Custom Services and Characteristics not support by HomeKit
-      this.service = this.accessory.getService(this.platform.CustomServices[accessory.context.device.CustomService]) ||
-        this.accessory.addService(this.platform.CustomServices[accessory.context.device.CustomService]);
-    } else {
-      this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-    }
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentHeatingCoolingState)
+        .onGet(this.handleCurrentHeatingCoolingStateGet.bind(this));
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+      this.service.getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
+        .onGet(this.handleTargetHeatingCoolingStateGet.bind(this))
+        .onSet(this.handleTargetHeatingCoolingStateSet.bind(this));
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
+      this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+        .onGet(this.handleCurrentTemperatureGet.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this)); // SET - bind to the `setBrightness` method below
+      this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature)
+        .onGet(this.handleTargetTemperatureGet.bind(this))
+        .onSet(this.handleTargetTemperatureSet.bind(this));
+
+      this.service.getCharacteristic(this.platform.Characteristic.TemperatureDisplayUnits)
+        .onGet(this.handleTemperatureDisplayUnitsGet.bind(this))
+        .onSet(this.handleTemperatureDisplayUnitsSet.bind(this));
 
     /**
      * Creating multiple services of the same type.
